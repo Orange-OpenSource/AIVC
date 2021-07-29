1fff9ea90e98b7f69d21f3563c46b43e07b1c80d
LADUNE Theo <theo.ladune@orange.com>
2021-07-29 11:58:14 +0200

    add part of the readme. Save before modifying the code to handle new models

--- src/sanity_script.sh
+++ src/sanity_script.sh
@@ -1,6 +1,14 @@
-#/bin/bash
-# export HOME=/opt/GPU/Exp/hkzv2358/
+#!/bin/bash
+# Required to ensure reproducibility between encoding and decoding
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
-# python encode.py -i ../raw_videos/BlowingBubbles_416x240_50_420.yuv --gop GOP_32 --end_frame 100 --start_frame 0 --model ms_ssim-5
-# python decode.py --model ms_ssim-5
-python evaluate.py --raw ../raw_videos/BlowingBubbles_416x240_50_420/ --compressed ../compressed/
+
+python aivc.py \
+    -i ../raw_videos/BlowingBubbles_416x240_50_420 \
+    -o ../compressed.yuv \
+    --bitstream_out ../bitstream.bin \
+    --start_frame 0 \
+    --end_frame 100 \
+    --coding_config RA \
+    --gop_size 16 \
+    --intra_period 32 \
+    --model ms_ssim-2021cc-1
