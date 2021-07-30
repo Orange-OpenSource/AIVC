913cda682881fb4cce93a150b52f1bad6fb99ab0
LADUNE Theo <theo.ladune@orange.com>
2021-07-30 16:30:48 +0200

    update readme

--- src/sanity_script.sh
+++ src/sanity_script.sh
@@ -4,11 +4,11 @@ export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
 python aivc.py \
     -i ../raw_videos/BlowingBubbles_416x240_50_420 \
-    -o ../compressed3.yuv \
-    --bitstream_out ../bitstream3.bin \
+    -o ../compressed.yuv \
+    --bitstream_out ../bitstream.bin \
     --start_frame 0 \
-    --end_frame 10 \
+    --end_frame 100 \
     --coding_config RA \
-    --gop_size 4 \
-    --intra_period 8 \
-    --model ms_ssim-2021cc-3
+    --gop_size 16 \
+    --intra_period 32 \
+    --model ms_ssim-2021cc-6
