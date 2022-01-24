fd673e3e442111744ab459543e5bf41c3f363b07
hkzv2358 <theo.ladune@orange.com>
2022-01-24 17:21:16 +0100

    wip first commit

---
+++ _site/sw.js
@@ -0,0 +1,73 @@
+const version = '20220124162025';
+const cacheName = `static::${version}`;
+
+const buildContentBlob = () => {
+  return ["/general/2016/08/29/example-post-three/","/history/external%20sources/2016/08/28/example-post-two/","/general/external%20sources/2016/08/27/example-post-one/","/categories/","/elements/","/blog/","/","/manifest.json","/offline/","/assets/search.json","/search/","/assets/styles.css","/thanks/","/redirects.json","/sitemap.xml","/robots.txt","/blog/page2/","/feed.xml","/assets/styles.css.map","/assets/logos/logo.svg", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  ]
+}
+
+const updateStaticCache = () => {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(buildContentBlob());
+  });
+};
+
+const clearOldCache = () => {
+  return caches.keys().then(keys => {
+    // Remove caches whose name is no longer valid.
+    return Promise.all(
+      keys
+        .filter(key => {
+          return key !== cacheName;
+        })
+        .map(key => {
+          console.log(`Service Worker: removing cache ${key}`);
+          return caches.delete(key);
+        })
+    );
+  });
+};
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    updateStaticCache().then(() => {
+      console.log(`Service Worker: cache updated to version: ${cacheName}`);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(clearOldCache());
+});
+
+self.addEventListener("fetch", event => {
+  let request = event.request;
+  let url = new URL(request.url);
+
+  // Only deal with requests from the same domain.
+  if (url.origin !== location.origin) {
+    return;
+  }
+
+  // Always fetch non-GET requests from the network.
+  if (request.method !== "GET") {
+    event.respondWith(fetch(request));
+    return;
+  }
+
+  // Default url returned if page isn't cached
+  let offlineAsset = "/offline/";
+
+  if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+    // If url requested is an image and isn't cached, return default offline image
+    offlineAsset = "/assets/default-offline-image.png";
+  }
+
+  // For all urls request image from network, then fallback to cache, then fallback to offline page
+  event.respondWith(
+    fetch(request).catch(async () => {
+      return (await caches.match(request)) || caches.match(offlineAsset);
+    })
+  );
+  return;
+});
--- index.md
+++ index.md
@@ -1,15 +1,26 @@
 ---
-title: About Alembic
+title: Orange AIVC
 feature_text: |
-  ## Alembic
-  A Jekyll boilerplate theme designed to be a starting point for any Jekyll website
+  ## AIVC
+  AIVC is an end-to-end neural video codec,  offering performance competitive with HEVC.
 feature_image: "https://picsum.photos/1300/400?image=989"
 excerpt: "Alembic is a starting point for [Jekyll](https://jekyllrb.com/) projects. Rather than starting from scratch, this boilerplate is designed to get the ball rolling immediately. Install it, configure it, tweak it, push it."
 ---
 
-Alembic is a starting point for [Jekyll](https://jekyllrb.com/) projects. Rather than starting from scratch, this boilerplate is designed to get rolling immediately. Install it, configure it, tweak it, push it.
+__AIVC__ is a **end-to-end neural video coder**, offering many practical features and great flexibility:
 
-{% include button.html text="Fork it" icon="github" link="https://github.com/daviddarnes/alembic" color="#0366d6" %} {% include button.html text="Buy me a coffee ☕️" link="https://buymeacoffee.com/daviddarnes#support" color="#f68140" %} {% include button.html text="Tweet it" icon="twitter" link="https://twitter.com/intent/tweet/?url=https://alembic.darn.es&text=Alembic%20-%20A%20Jekyll%20boilerplate%20theme&via=DavidDarnes" color="#0d94e7" %} {% include button.html text="Install Alembic ⚗️" link="https://github.com/daviddarnes/alembic#installation" %}
+| __Features__                | __What's inside?__  |
+| :---:                   | :-: |
+| Coding configurations   | Random Access, Low-delay P & All Intra |
+| Tunable Intra Period    | Up to 64
+| Tunable GOP size        | Up to 64
+| 7 Different rates target   | 1 MBit/s to 20 MBit/s (for 1080p videos)
+| Competitive performances| On par with HM (HEVC Test Model)
+| Convenient input format | 8-bit YUV 4:2:0 videos
+
+---
+
+{% include button.html text="Get the models" icon="github" link="https://github.com/Orange-OpenSource/AIVC" color="#0366d6" %} {% include button.html text="Read the paper ☕️" link="(https://arxiv.org/abs/2104.09103" color="#f68140" %} {% include button.html text="See some slides" link="https://theoladune.github.io/AIVC-Seminar/" color="#0d94e7" %}
 
 ## Features
 
